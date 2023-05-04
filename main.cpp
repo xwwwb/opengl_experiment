2184328c7aae09b697b0abf8094b8b562f695d97
xwwwb <xuwenbo@88.com>
2023-05-04 21:03:16 +0800

    完成扫描线填充算法

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
-#include <vector>
-#include <stack>
 #include <queue>
+#include <stack>
+#include <vector>
 
 using namespace std;
 
 class Point {
-public :
+public:
     int x;
     int y;
 
@@ -18,17 +18,21 @@ public :
 };
 
 vector<vector<Point>> *points;
-vector<Point> fill_points;
 GLFWwindow *window;
 float xscale, yscale;
 
+const int method = 1;// 0: 种子 1: 扫描线
+
 int OpenGLInit();
-
+void drawLine();
+void drawPoins();
+void SinglePoint(int, int);
 void mouseHandler(GLFWwindow *, int, int, int);
-
 void keyHandler(GLFWwindow *, int, int, int, int);
+void read_color(int x, int y, unsigned char *pixel);
+void flood_fill(int, int);
+void scanning_strip(int, int);
 
-void flood_fill(int x, int y);
 
 int main() {
     OpenGLInit();
@@ -41,14 +45,12 @@ int main() {
     }
 }
 
-
 int OpenGLInit() {
     if (!glfwInit()) {
         return -1;
     }
     // 创建一个窗口
     window = glfwCreateWindow(640, 480, "Learn OpenGL", nullptr, nullptr);
-
     if (!window) {
         glfwTerminate();
         return -1;
@@ -61,9 +63,6 @@ int OpenGLInit() {
 
     points = new vector<vector<Point>>();
     points->push_back(*(new vector<Point>()));
-
-    fill_points = *(new vector<Point>());
-
     GLFWmonitor *pMonitor = glfwGetPrimaryMonitor();
     glfwGetMonitorContentScale(pMonitor, &xscale, &yscale);
     return 0;
@@ -74,8 +73,8 @@ void drawLine() {
         if (points_.size() <= 1 || (points_.front().x == -100 && points_.front().y == -100)) {
             break;
         }
-        // #03A9F4
         glColor3f(0.01, 0.66, 0.96);
+        //        glColor3f(0.9, 0, 0);
         glLineWidth(5.0);
         glBegin(GL_LINE_LOOP);
         for (int i = 1; i < points_.size(); i++) {
@@ -83,7 +82,6 @@ void drawLine() {
         }
         glEnd();
     }
-
 }
 
 void drawPoints() {
@@ -91,7 +89,6 @@ void drawPoints() {
         if (points_.size() <= 1) {
             break;
         }
-        // #03A9F4
         glColor3f(0.01, 0.66, 0.96);
         glPointSize(5.0);
         glBegin(GL_POINTS);
@@ -103,13 +100,21 @@ void drawPoints() {
 }
 
 void SinglePoint(int x, int y) {
+
     glColor3f(0.9, 0.0, 0.0);
     glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glVertex2f(x + 0.1f, y + 0.1f);
-    glVertex2f(x - 0.1f, y - 0.1f);
-    glVertex2f(x + 0.1f, y - 0.1f);
-    glVertex2f(x - 0.1f, y + 0.1f);
+    if (!method) {
+        glPointSize(5.0);
+        glVertex2i(x, y);
+        glVertex2f(x + 0.1f, y + 0.1f);
+        glVertex2f(x - 0.1f, y - 0.1f);
+        glVertex2f(x + 0.1f, y - 0.1f);
+        glVertex2f(x - 0.1f, y + 0.1f);
+    } else {
+        glPointSize(1.0);
+        glVertex2i(x, y);
+    }
+
     glEnd();
     glfwSwapBuffers(window);
 }
@@ -120,17 +125,16 @@ void mouseHandler(GLFWwindow *window_, int button, int action, int mods) {
         double x, y;
         glfwGetCursorPos(window_, &x, &y);
         if (points->back().empty()) {
-            points->back().emplace_back(-100, -100); // 使用-100 标记没有被绘制
+            points->back().emplace_back(-100, -100);// 使用-100 标记没有被绘制
             points->back().emplace_back(int(x), int(y));
         } else {
             auto *point = &points->back();
             point->push_back(Point(int(x), int(y)));
         }
-
     }
     if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
         if (points->back().size() > 1) {
-            points->back().front().x = -10; // 使用-10来标记 需要被绘制
+            points->back().front().x = -10;// 使用-10来标记 需要被绘制
             points->back().front().y = -10;
             points->push_back(*(new vector<Point>()));
         }
@@ -145,8 +149,20 @@ void keyHandler(GLFWwindow *window_, int key, int scancode, int action, int mods
         // 拿到鼠标位置
         double xpos, ypos;
         glfwGetCursorPos(window_, &xpos, &ypos);
-        flood_fill(int(xpos), int(ypos));
+        if (method) {
+            scanning_strip(int(xpos), int(ypos));
+        } else {
+            flood_fill(int(xpos), int(ypos));
+        }
     }
+}
+
+void read_color(int x, int y, unsigned char *pixel) {
+    int x_ = int(double(x) * xscale);
+    int y_ = int(double(480 - y) * yscale);
+    int w = int(1 * xscale);
+    int h = int(1 * yscale);
+    glReadPixels(x_, y_, w, h, GL_RGB, GL_UNSIGNED_BYTE, pixel);
 }
 
 void flood_fill(int xpos, int ypos) {
@@ -154,7 +170,6 @@ void flood_fill(int xpos, int ypos) {
     fill_stack.push(new Point(xpos, ypos));
     // 保存当前像素
     unsigned char pixel[3];
-
     while (!fill_stack.empty()) {
         Point *point = fill_stack.top();
         fill_stack.pop();
@@ -164,29 +179,128 @@ void flood_fill(int xpos, int ypos) {
         if (x <= 1 || x >= 639 || y <= 1 || y >= 479) {
             continue;
         }
-
-        int x_ = int(double(x) * xscale);
-        int y_ = int(double(480 - y) * yscale);
-        int w = int(1 * xscale);
-        int h = int(1 * yscale);
-
-        glReadPixels(x_, y_, w, h, GL_RGB, GL_UNSIGNED_BYTE, pixel);
-
-//        if (pixel[0] == 3 && pixel[1] == 168 && pixel[2] == 245) {
-//            continue;
-//        }
+        read_color(x, y, pixel);
         if (pixel[0] != 255 && pixel[1] != 255 && pixel[2] != 255) {
             continue;
         }
         SinglePoint(x, y);
         fill_stack.push(new Point(x + 2, y));
-//        fill_stack.push(new Point(x + 1, y + 1));
         fill_stack.push(new Point(x - 2, y));
-//        fill_stack.push(new Point(x + 1, y - 1));
         fill_stack.push(new Point(x, y + 2));
-//        fill_stack.push(new Point(x - 1, y + 1));
         fill_stack.push(new Point(x, y - 2));
-//        fill_stack.push(new Point(x - 1, y - 1));
-        cout << "flood fill" << endl;
+    }
+}
+
+void scanning_strip(int xpos, int ypos) {
+    stack<Point *> fill_stack;
+    fill_stack.push(new Point(xpos, ypos));
+    unsigned char pixel[3];
+
+    while (!fill_stack.empty()) {
+        Point *point = fill_stack.top();   // 取当前栈顶元素为种子点
+        fill_stack.pop();                  // 删除当前栈顶元素
+        int l_x = point->x, r_x = point->x;// 向右行驶到最大的x 和向左行驶到最大的x
+        while (r_x <= 640) {
+            // 向右行驶
+            read_color(r_x, point->y, pixel);                                        // 读取颜色
+            if (int(pixel[0]) == 3 && int(pixel[1]) == 168 && int(pixel[2]) == 245) {// 碰到了边界
+                break;
+            }
+
+            SinglePoint(r_x, point->y);
+            r_x++;
+        }
+        while (l_x >= 0) {
+            read_color(l_x, point->y, pixel);
+            if (int(pixel[0]) == 3 && int(pixel[1]) == 168 && int(pixel[2]) == 245) {// 碰到了边界
+                break;
+            }
+            SinglePoint(l_x, point->y);// 向左绘制
+            l_x--;
+        }
+
+        int b_time = 0;
+        int b_x = r_x, b_y = point->y + 2;
+        while (b_x >= 0 && b_time < 6) {
+            int result = 0;
+            read_color(b_x, b_y, pixel);                                             // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(b_x + 1, b_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(b_x - 1, b_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(b_x + 2, b_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(b_x - 2, b_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            if (result == 5) {
+                fill_stack.push(new Point(b_x, b_y));
+                break;
+            }
+            b_x -= 1;// 向右寻找可用点
+            b_time++;
+        }
+        // 向上寻找
+        int u_time = 0;
+        int u_x = r_x, u_y = point->y - 2;
+        while (u_x >= 0 && u_time < 6) {
+            int result = 0;
+            read_color(u_x, u_y, pixel);                                             // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(u_x + 1, u_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(u_x - 1, u_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(u_x + 2, u_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            read_color(u_x - 2, u_y, pixel);                                         // 检查自身是否为边界
+            if (int(pixel[0]) != 3 && int(pixel[1]) != 168 && int(pixel[2]) != 245) {// 不为边界
+                if (int(pixel[0]) != 229 && int(pixel[1] != 0 && pixel[2] != 0)) {
+                    result++;
+                }
+            }
+            if (result == 5) {
+                fill_stack.push(new Point(u_x, u_y));
+                break;
+            }
+            u_x -= 1;// 向右寻找可用点
+            u_time++;
+        }
     }
 }
